5c87e7159fa2b3e4efceaf7725659c4fef4efb90
Jack Power <2012jackpower@gmail.com>
2021-11-17 21:35:00 +0000

    Added routing for reviews.

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
+import genresRouter from './api/genres';
 
 dotenv.config();
 
@@ -12,6 +13,7 @@ app.use(express.json());
 
 app.use('/api/movies', moviesRouter);
 
+app.use('/api/genres', genresRouter);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
